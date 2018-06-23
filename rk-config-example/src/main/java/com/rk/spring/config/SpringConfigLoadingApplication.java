fd11b06e3adb9c6f6c2a2b04989d8075e51188b3
Riddhiman Karmakar <riddhiman.karmakar@gmail.com>
2018-06-24 02:45:01 +0530

    Printed all available beans

--- rk-config-example/src/main/java/com/rk/spring/config/SpringConfigLoadingApplication.java
+++ rk-config-example/src/main/java/com/rk/spring/config/SpringConfigLoadingApplication.java
@@ -1,5 +1,7 @@
 package com.rk.spring.config;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -8,6 +10,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class SpringConfigLoadingApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringConfigLoadingApplication.class, args);
+		System.out.println("################### BEANS ##################");
+		Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(System.out::println);
+		System.out.println("############# END BEAN DEFINITION ##########");
+		System.out.println(" ");
+		System.out.println(" ");
 		System.out.println("############################################");
 		System.out.println("First Name: " + applicationContext.getBean("firstName"));
 		System.out.println("Last Name: " + applicationContext.getBean("lastName"));
